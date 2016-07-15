f48f8fd3a22453d49d4eed4994b6d36cfe0b9b7b
Rameez <meerdara@gmail.com>
2016-07-15 01:35:05 -0400

    Added comments to shell script

--- mvnScr.sh
+++ mvnScr.sh
@@ -4,4 +4,9 @@ mvn -Dtest=JunitTestSuite test
 rc=$?
 if [[ $rc -ne 0 ]] ; then
 	echo 'could not perform tests'; exit $rc
+	#if test passes, merge it to master
+	#switch to master first
+       	
+	#git checkout master
+	#git merge testing
 fi
